ce36e220dae2194c5543269d4f856f5db3d9ad08
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-11-29 15:51:06 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,80 @@
-return (
-  <>
-    <header className="droppa-header">
-      {/* Location Picker */}
-      <div className="location-picker" onClick={toggleLocationDropdown}>
-        <FiMapPin className="map-pin" size={18} />
-        <span className="address">{location}</span>
-        <FiChevronDown size={16} className={`arrow ${showLocationDropdown ? "open" : ""}`} />
+import React, { useState } from "react";
+import { FiMapPin, FiFilter, FiChevronDown } from "react-icons/fi";
+import "../styles/header.css";
 
-        {showLocationDropdown && (
-          <div className="location-dropdown">
-            {locations.map((loc) => (
-              <div key={loc} className="location-item" onClick={() => selectLocation(loc)}>
-                {loc}
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+export default function Header() {
+  const [location, setLocation] = useState("Lagos");
+  const [showLocationDropdown, setShowLocationDropdown] = useState(false);
+  const [showFilters, setShowFilters] = useState(false);
 
-      {/* Filter Button */}
-      <div className="header-right">
-        <button className="filter-btn" onClick={toggleFilters}>
-          <FiFilter size={20} /> Filters
-        </button>
-      </div>
-    </header>
+  const locations = ["Lagos", "Abuja", "Port Harcourt", "Kano"];
+  const filters = [
+    "Fast Delivery",
+    "Rating 4.5+",
+    "Near Me",
+    "Promos",
+    "Discounts",
+    "Top Rated",
+    "New",
+    "Popular",
+  ];
 
-    {/* --- Filters Dropdown UNDER header (not inside header) --- */}
-    {showFilters && (
-      <div className="filters-dropdown-under">
-        {filters.map((filter) => (
-          <div key={filter} className="filter-item">
-            {filter}
-          </div>
-        ))}
-      </div>
-    )}
-  </>
-);
+  const toggleLocationDropdown = () =>
+    setShowLocationDropdown(!showLocationDropdown);
+
+  const selectLocation = (loc) => {
+    setLocation(loc);
+    setShowLocationDropdown(false);
+  };
+
+  const toggleFilters = () => setShowFilters(!showFilters);
+
+  return (
+    <>
+      {/* --------- HEADER --------- */}
+      <header className="droppa-header">
+        {/* Location Picker */}
+        <div className="location-picker" onClick={toggleLocationDropdown}>
+          <FiMapPin className="map-pin" size={18} />
+          <span className="address">{location}</span>
+          <FiChevronDown
+            size={16}
+            className={`arrow ${showLocationDropdown ? "open" : ""}`}
+          />
+
+          {showLocationDropdown && (
+            <div className="location-dropdown">
+              {locations.map((loc) => (
+                <div
+                  key={loc}
+                  className="location-item"
+                  onClick={() => selectLocation(loc)}
+                >
+                  {loc}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+
+        {/* Filter Button */}
+        <div className="header-right">
+          <button className="filter-btn" onClick={toggleFilters}>
+            <FiFilter size={20} /> Filters
+          </button>
+        </div>
+      </header>
+
+      {/* --------- FILTERS DROPDOWN (Next Line) --------- */}
+      {showFilters && (
+        <div className="filters-dropdown-under">
+          {filters.map((filter) => (
+            <div key={filter} className="filter-item">
+              {filter}
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
