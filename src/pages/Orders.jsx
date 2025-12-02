5c5c95f3adcbc16d11af77b040470d32d5b7d949
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-12-02 07:18:29 +0100

    Update Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -14,7 +14,7 @@ export default function OrderPage() {
       
       {/* Sticky Tabs */}
       <div className="order-tabs-wrapper">
-        <h2 clasName="order-header">Your Orders<h2/>
+        <h2 className="order-header">Your Orders</h2>
         <div className="order-tabs">
           <div
             className={`order-tab ${activeTab === "cart" ? "active" : ""}`}
@@ -81,5 +81,6 @@ function EmptyHistory() {
     </div>
   );
 }
+
 
 
