8e48b8e6fd33c8850a46d075e49dfcfa0a32a134
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-12-07 17:40:33 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,30 @@
-// import React from "react";
-// import "../styles/login.css";
-// import { FiArrowRight } from "react-icons/fi";
+import React from "react";
+import "../styles/login.css";
+import { FiArrowRight } from "react-icons/fi";
 
-// export default function LoginPage() {
-//   return (
-//     <div className="login-container">
+export default function LoginPage() {
+  return (
+    <div className="lp-container">
       
-//       {/* Greeting */}
-//       <h1 className="login-greeting">Welcome Back </h1>
-//       <p className="login-subtext">Enter your phone number to continue</p>
+      {/* Greeting */}
+      <h1 className="lp-greeting">Welcome Back</h1>
+      <p className="lp-subtext">Enter your phone number to continue</p>
 
-//       {/* Phone Input */}
-//       <div className="login-input-group">
-//         <label className="login-label">Phone Number</label>
-//         <input 
-//           type="tel"
-//           className="login-input"
-//           placeholder="e.g. 08123456789"
-//         />
-//       </div>
+      {/* Phone Input */}
+      <div className="lp-input-group">
+        <label className="lp-label">Phone Number</label>
+        <input 
+          type="tel"
+          className="lp-input"
+          placeholder="e.g. 08123456789"
+        />
+      </div>
 
-//       {/* Button */}
-//       <button className="login-btn">
-//         Continue <FiArrowRight className="arrow" />
-//       </button>
+      {/* Button */}
+      <button className="lp-btn">
+        Continue <FiArrowRight className="lp-arrow" />
+      </button>
 
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
