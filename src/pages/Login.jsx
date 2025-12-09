ef487123b6ee6005be0256ef5b09f5418b05b460
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-12-09 10:13:06 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/login.css";
+import "../styles/login.css"; 
 import { FiArrowRight } from "react-icons/fi";
 
 export default function LoginPage() {
