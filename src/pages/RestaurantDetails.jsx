68adf1a89427117c6b5f699f3eb4105f2f7cb20b
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-12-17 20:03:06 +0100

    Update RestaurantDetails.jsx

--- src/pages/RestaurantDetails.jsx
+++ src/pages/RestaurantDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import {
+import { 
   featuredRestaurants,
   restaurantMenus,
   getRestaurantTimeDisplay,
