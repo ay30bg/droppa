8bbef7d0aa68433787d0d33d9790927f474f2684
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-12-20 07:38:25 +0100

    Update OrdersHeader.jsx

--- src/components/OrdersHeader.jsx
+++ src/components/OrdersHeader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/orders.css";
-import "../styles/orderheader.css"
+import "../styles/orderheader.css";
 
 export default function OrdersHeader({
   activeTab,
