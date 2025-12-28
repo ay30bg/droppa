2d58eebd8ed0bde017f61b54a1884fb79ef4fd03
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-12-28 08:37:40 +0100

    Update PersonalDetails.jsx

--- src/pages/PersonalDetails.jsx
+++ src/pages/PersonalDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { FiArrowLeft, FiSave } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
-import "../styles/personalInfo.css";
+import "../styles/personaldetails.css";
 
 export default function PersonalInformationPage() {
   const navigate = useNavigate();
